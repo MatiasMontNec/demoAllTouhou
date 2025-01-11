f652d333619438f5a4c85aacbbff0466fbce8f7f
MatiasMontNec <matias.montano@usach.cl>
2025-01-11 14:47:27 -0300

    Initializer Commit

---
+++ src/main/java/com/example/demoAllTouhou/DemoAllTouhouApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAllTouhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAllTouhouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAllTouhouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoAllTouhou/DemoAllTouhouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAllTouhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAllTouhouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
